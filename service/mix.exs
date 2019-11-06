a35de7df248d19f40a1b18245f02346cf85012d0
Felipe Rodrigues <felipe@crafters.cc>
2019-11-06 13:30:14 -0300

    Adding Credo and ExMachina to the deps

--- service/mix.exs
+++ service/mix.exs
@@ -22,6 +22,9 @@ defmodule ChattermillReviewService.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:ex_machina, "~> 2.3", only: :test}
+    ]
   end
 end
