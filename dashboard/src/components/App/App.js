e662211263c05efa8dd4e76dd7da31623079c892
Felipe Rodrigues <felipe@crafters.cc>
2019-11-11 15:57:23 -0300

    Moving App.js to the components directory and cleaning it up

---
+++ dashboard/src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <span>Reviews dashboard</span>
+    </div>
+  );
+}
+
+export default App;
